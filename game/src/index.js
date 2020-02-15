41a8b2c713316765b84f951e5be538be0c95a485
Brockerboy <brockgib@gmail.com>
2020-02-15 11:21:36 -0600

    Continue initial set up of game folders and files

---
+++ game/src/index.js
@@ -0,0 +1,9 @@
+//@flow
+// Main composition plan for the pacman game service.
+
+export const handle
+  = event => {
+    console.log('handle', event)
+
+    return Promise.resolve({})
+  }
---
+++ game/test/index.js
@@ -0,0 +1,6 @@
+import { describe, it } from "mocha";
+
+describe("Developer: There is no I in TEAM", () => {
+  it("Tester: We cannot spell BUGS without U", () => {});
+  it("TODO: Write tests here someday", () => {});
+});
